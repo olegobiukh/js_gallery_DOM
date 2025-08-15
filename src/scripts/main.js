d7ec4a704a8dbbae1cb1cf95a1eb94f5a65072ce
Oleh <oleh.obiukh@gmail.com>
2025-08-15 12:53:59 +0200

    image gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+
+document.querySelectorAll('.gallery__list').forEach((item) => {
+  item.style.display = 'flex';
+  item.style.flexWrap = 'wrap';
+});
+
+document.querySelectorAll('.list-item').forEach((item) => {
+  // eslint-disable-next-line no-shadow
+  item.onclick = (event) => {
+    event.preventDefault();
+
+    const link = item.querySelector('.list-item__link');
+
+    largeImg.src = link.href;
+  };
+});
